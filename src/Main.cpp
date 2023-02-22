d9c9eda7df177d2704051bba9d0c6bf11b8a9a29
Asiern <asiern.dev@gmail.com>
2023-02-22 09:42:39 +0100

    set: 0.2 version as default

--- src/Main.cpp
+++ src/Main.cpp
@@ -56,9 +56,9 @@ wxBEGIN_EVENT_TABLE(Main, wxFrame) EVT_CLOSE(Main::OnClose) wxEND_EVENT_TABLE()
       m_gameVer->SetFont(font);*/
 
     wxArrayString* versions = new wxArrayString();
-    versions->Insert("Auto", 0);
+    versions->Insert("0.2", 0);
     versions->Insert("0.1", 1);
-    versions->Insert("0.2", 2);
+    versions->Insert("Auto", 2);
     wxComboBox* m_version_selector =
         new wxComboBox(this, wxID_ANY, "Auto", wxPoint(width - margin * 5, margin * 3), wxDefaultSize, *versions,
                        wxCB_READONLY | wxCB_SIMPLE, wxDefaultValidator, wxComboBoxNameStr);
